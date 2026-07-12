220af37c7dee387716681c9bbc1ee5351c5d0fad
binary_soup <contact@binarysoup.dev>
2026-07-12 12:40:12 -0400

    Fix errors chain docs

--- errors/chain.go
+++ errors/chain.go
@@ -9,12 +9,12 @@ func SetChainSeparator(sep string) {
 	separator = sep
 }
 
-// Create a new error with the given message, then chain behind the first error.
+// Chain the error behind a new message.
 func Chain(err error, msg string) error {
 	return Format("%s%s%s", msg, separator, err)
 }
 
-// Format the message with fmt to create a new error, then chain behind the first error.
+// Chain the error behind a new message formatted it with fmt.
 func ChainFormat(err error, format string, a ...any) error {
 	return Chain(err, fmt.Sprintf(format, a...))
 }
